bc681aa441e04e596849744a7cc6093f7846e5b8
47-hlj <1725816370@qq.com>
2021-06-16 14:21:10 +0800

    实现用户登录操作

--- jt/src/main/java/com/jt/service/UserServiceImpl.java
+++ jt/src/main/java/com/jt/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.jt.service;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.jt.mapper.UserMapper;
 import com.jt.pojo.User;
+import com.jt.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
@@ -15,7 +16,7 @@ import java.util.UUID;
  * 时间 2021/5/11
  */
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserMapper userMapper;
