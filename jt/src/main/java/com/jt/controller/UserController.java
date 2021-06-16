0967cddac9c62ae201fcd9d7315c3e326610810a
47-hlj <1725816370@qq.com>
2021-06-16 09:18:34 +0800

    完成用户登录操作

--- jt/src/main/java/com/jt/controller/UserController.java
+++ jt/src/main/java/com/jt/controller/UserController.java
@@ -2,10 +2,10 @@ package com.jt.controller;
 
 import com.jt.pojo.User;
 import com.jt.service.UserService;
+import com.jt.vo.SysResult;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,14 +15,33 @@ import java.util.List;
  */
 @RestController
 @CrossOrigin
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping("/hello")
-    public List<User> hello(){
+    /**
+     * 需求: 根据用户名和密码实现用户登录,要求返回token
+     * URL: /user/login
+     * 请求参数: post  JSON {username:"xxx",password:"xxxx"}
+     * 返回值: SysResult对象 (token)
+     */
 
-        return userService.findAll();
+    @PostMapping("/login")
+    public SysResult login(@RequestBody User user){
+        //执行后端登录操作，要求返回token密钥
+        String token = userService.login(user);
+        //判断token返回值是否有效
+        if(StringUtils.hasLength(token)){
+            return SysResult.success(token);
+        } else
+        {
+          //登录失败
+          return SysResult.fail();
+        }
+
     }
+
+
 }
--- jt/src/main/java/com/jt/service/UserServiceImpl.java
+++ jt/src/main/java/com/jt/service/UserServiceImpl.java
@@ -1,11 +1,14 @@
 package com.jt.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.jt.mapper.UserMapper;
 import com.jt.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * @author 刘昱江
@@ -20,5 +23,34 @@ public class UserServiceImpl implements UserService{
     @Override
     public List<User> findAll() {
         return userMapper.selectList(null);
+    }
+
+    /**
+     * 实现用户登录操作
+     * 1.将明文转化为密文 MD5加密
+     * 2.通过username和password查询数据库.
+     * 3.
+     *     有数据: 用户名和密码正确  返回UUID token
+     *     没有数据: 用户名和密码错误 返回null
+     * @param user
+     * @return
+     */
+    @Override
+    public String login(User user) {
+        String password = user.getPassword();
+        //md5hash相对于md5更加"安全" 没有绝对安全的系统!!!
+        String md5Pass = DigestUtils.md5DigestAsHex(password.getBytes());
+        user.setPassword(md5Pass);
+        //根据其中不为null的数据当做where条件 u/p
+        //Sql: select * from user where u=#{u} and p=#{p}
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>(user);
+        User userDB = userMapper.selectOne(queryWrapper);
+        if(userDB == null){
+            //没有查询到数据
+            return null;
+        }
+        String token = UUID.randomUUID().toString().replace("-","");
+        return token;
+
     }
 }
